89a452b4ae1ff075a09999c8e796b83d9f64a14e
Sunil Kumar <SunilKumar@bluelightcard.co.uk>
2025-04-14 16:58:57 +0530

    Code refactor

--- flatlistdemo/Components/GoalInput.js
+++ flatlistdemo/Components/GoalInput.js
@@ -1,9 +1,28 @@
 
-function GoalInput() {
+import { View, Button, TextInput, StyleSheet } from "react-native";
+import { useState } from "react";
+
+function GoalInput(props) {
+    const [enteredGoalText, setEnteredGoalText] = useState('');
+
+    function goalInputHandler(enteredGoal) {
+        console.log(`enteredGoal goal is ${enteredGoal}`)
+        setEnteredGoalText(enteredGoal)
+      }
+
+      function InputHandler(){
+        props.inputHandler(enteredGoalText);
+      }
+
 return (
          <View style={styles.inAppContainer}>
-          <TextInput placeholder='Your course goal' style={styles.inputText} onChangeText={goalInputHandler}/>
-          <Button title='Add goal' onPress={addGoalHandler}/>
+          <TextInput
+           placeholder='Your course goal' 
+          style={styles.inputText} 
+          onChangeText={goalInputHandler}/>
+          <Button 
+          title='Add goal' 
+          onPress={InputHandler}/>
          </View>
 )
 }
@@ -12,3 +31,24 @@ return (
 export default GoalInput;
 
 
+const styles = StyleSheet.create(
+    {
+        inAppContainer: {
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            marginBottom: 24,
+            borderBottomWidth: 1,
+            borderBottomColor: '#cccccc',
+            flex: 1,
+          },
+          inputText: {
+            borderWidth: 1,
+            borderColor: '#cccccc',
+            width: '75%',
+            padding: 10,
+            marginRight: 8,
+          },
+
+    }
+)
